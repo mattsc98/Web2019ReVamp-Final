a86a4bc574de9920f148fea16ed163ff7d2cd375
Matthew Schembri <mattsc98@gmail.com>
2019-05-22 23:05:39 +0200

    -ManageUsersController

---
+++ Web2019ReVamp/Models/ManageUsersViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web2019ReVamp.Data;
+
+namespace Web2019ReVamp.Models
+{
+    public class ManageUsersViewModel
+    {
+        public ApplicationUser[] Administrators { get; set; }
+
+        public ApplicationUser[] Everyone { get; set; }
+    }
+}
