888831e0e8b388341788852a32db6b464485e81f
Matthew Schembri <mattsc98@gmail.com>
2019-05-27 12:59:42 +0200

    -Edited DbInitilizer

--- Web2019ReVamp/Models/DbInitializer.cs
+++ Web2019ReVamp/Models/DbInitializer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Web2019ReVamp.Data;
 
@@ -24,9 +25,47 @@ namespace Web2019ReVamp.Models
                     roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
                 }
             }
-                
 
+            var roleManager = serviceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
+            await EnsureRolesAsync(roleManager);
 
+            var userManager = serviceProvider
+                .GetRequiredService<UserManager<ApplicationUser>>();
+            await EnsureTestAdminAsync(userManager);
+
+        }
+
+        private static async Task EnsureRolesAsync(
+            RoleManager<IdentityRole> roleManager)
+        {
+            var alreadyExists = await roleManager
+                .RoleExistsAsync(Constants.AdministratorRole);
+
+            if (alreadyExists) return;
+
+            await roleManager.CreateAsync(
+                new IdentityRole(Constants.AdministratorRole));
+        }
+
+        private static async Task EnsureTestAdminAsync(
+            UserManager<ApplicationUser> userManager)
+        {
+            var testAdmin = await userManager.Users
+                .Where(x => x.UserName == "AdminSuper")
+                .SingleOrDefaultAsync();
+
+            if (testAdmin != null) return;
+
+            testAdmin = new ApplicationUser
+            {
+                UserName = "AdminSuper",
+                Email = "admin@super.local"
+            };
+            await userManager.CreateAsync(
+                testAdmin, "Admin@123");
+            await userManager.AddToRoleAsync(
+                testAdmin, Constants.AdministratorRole);
         }
 
     }
